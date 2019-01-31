262341e01885af51520f22deef897c834e0bf272
Janning Vygen <vygen@kicktipp.de>
2019-01-31 10:22:29 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
-    <div>
+  <div>
     <quiz-title></quiz-title>
     <quiz-menu></quiz-menu>
     <quiz-frage></quiz-frage>
-    </div>
+  </div>
 </template>
 
 <script>
-import Title from './components/Title.vue'
-import Frage from './components/Fragen.vue'
-import Menu from './components/StarterMenu.vue'
-import {eventBus} from './main.js'
-export default {
-    data: function() {
-        return {
-            aktuelleFrage: 0
-        }
+  import Title from './components/Title.vue'
+  import Frage from './components/Fragen.vue'
+  import Menu from './components/StarterMenu.vue'
+  import {eventBus} from './main.js'
+
+  export default {
+    data: function () {
+      return {
+        aktuelleFrage: 0
+      }
     },
     components: {
-        quizTitle: Title,
-        quizFrage: Frage,
-        quizMenu: Menu
+      quizTitle: Title,
+      quizFrage: Frage,
+      quizMenu: Menu
     },
     created() {
-        eventBus.$on('gameStopped', () => {
-            this.aktuelleFrage = 0
-        })
+      eventBus.$on('gameStopped', () => {
+        this.aktuelleFrage = 0
+      })
     }
-}
+  }
 </script>
 
 <style>
