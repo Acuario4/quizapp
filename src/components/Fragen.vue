ad85e700e2ed2aab64766ceedc6a97ff2777f704
lennart <lenn.art@aol.de>
2019-02-01 15:34:18 +0100

    New Quiz array (Unincluded in code)

--- src/components/Fragen.vue
+++ src/components/Fragen.vue
@@ -31,7 +31,7 @@ export default {
                 {id: 19, Richtig: 4, Frage: "Das Hexadezimalsystem ist ein Stellenwertsystem zur Basis ..."},
                 {id: 110, Richtig: 3, Frage: "Rechne folgende Hexadezimalzahl in eine Dezimalzahl um: 1BF6ₕₑₓ"},
                 {id: 21, Richtig: 3, Frage: "Was ist die Summe der Zahlen von 1 bis 10?"},
-                {id: 22, Richtig: 4, Frage: "Sie stapeln fünf Würfel übereinander zu einem Turm. Die Augenzahl auf der Oberseite des obersten Würfels ist sechs. Wie lautet die Summe aller Augenzahlen?"},
+                {id: 22, Richtig: 4, Frage: "Sie stapeln fünf Würfel übereinander zu einem Turm. Die Augenzahl auf der Oberseite des obersten Würfels ist sechs. Wie lautet die Summe aller sichtbaren Augenzahlen?"},
                 {id: 23, Richtig: 3, Frage: "Es ist Punkt 14 Uhr. In welchem Winkel stehen großer und kleiner Zeiger in diesem Moment zueinander?"},
                 {id: 24, Richtig: 1, Frage: "300 g Erdbeeren kosten 1,95 €. Wie viel kosten 500 g?"},
                 {id: 25, Richtig: 1, Frage: "Das Ergebnis, wenn man alle Zahlen von 1 bis 100 addiert, lautet..."},
@@ -43,6 +43,70 @@ export default {
                 
 
             ],
+            Quiz : [{quizId:1, name: "Informatik", fragen: [
+                {text: "Was ist der Affengriff für Windows-Nutzer?", antworten: [
+                    "[Strg] + [Alt] + [Shift]","[Strg] + [Alt] + [Entf]","[Strg] + [Alt] + [Einfg]","[Strg] + [Alt] + [Esc]"
+                ], richtig: 2},
+                {text: "Shareware ist ...", antworten: [
+                    "... eine Software, die sich mehrere Nutzer teilen können.","... die Auswahl preiswerter Aktienindex-Fonds durch ein Computersystem.","... eine Cloud, die die Verbreitung von besonders vielen Daten zulässt.","... eine Software, die als kostenlose Testversion zur Verfügung gestellt wird."
+                ], richtig: 4},
+                {text: "Im Binärsystem gibt es ...", antworten: [
+                    "... alle Zahlen von 0 bis 9.","... nur die Ziffern 0 und 1.","... alle Zahlen von 1 bis 8","... nur die Zahlen 10 und 11."
+                ], richtig: 2},
+                {text: "Was ist ein Bit?", antworten: [
+                    "Eine Folge aus 8 Ziffern","Eine Folge aus 16 Ziffern","Eine Binärziffer","Eine Kombination nur aus Ziffern des Binärsystems"
+                ], richtig: 3},
+                {text: "Wie viele Zustände kann man mit zwei Bits herstellen?", antworten: [
+                    "2","4","6","8"
+                ], richtig: 2},
+                {text: "Weiter im Binär-/Dualsystem. Wie lautet die nächste Zahl in dieser Reihe? 0, 1, 10, 11, 100, 101, 110, 111, 1000, ...", antworten: [
+                    "1001","1010","1100","1101"
+                ], richtig: 1},
+                {text: "Dualsystem: 1111 -Dezimalsystem: ...", antworten: [
+                    "1","4","7","15"
+                ], richtig: 4},
+                {text: "Dualsystem: 10011 -Dezimalsystem: ...", antworten: [
+                    "3","19","25","37"
+                ], richtig: 2},
+                {text: "Das Hexadezimalsystem ist ein Stellenwertsystem zur Basis ...", antworten: [
+                    "6","8","10","16"
+                ], richtig: 4},
+                {text: "Rechne folgende Hexadezimalzahl in eine Dezimalzahl um: 1BF6ₕₑₓ", antworten: [
+                    "32","1080","7158","23445"
+                ], richtig: 3}
+            ]},
+            {quizId:2, name: "Mathematik", fragen: [
+                {text:"Was ist die Summe der Zahlen von 1 bis 10?", antworten: [
+                    "48","50","55","58"
+                ], richtig: 3},
+                {text:"Sie stapeln fünf Würfel übereinander zu einem Turm. Die Augenzahl auf der Oberseite des obersten Würfels ist sechs. Wie lautet die Summe aller sichtbaren Augenzahlen?", antworten: [
+                    "61","66","71","76"
+                ], richtig: 4},
+                {text:"Es ist Punkt 14 Uhr. In welchem Winkel stehen großer und kleiner Zeiger in diesem Moment zueinander?", antworten: [
+                    "45 Grad","55 Grad","60 Grad","66.6 Grad"
+                ], richtig: 3},
+                {text:"300 g Erdbeeren kosten 1,95 €. Wie viel kosten 500 g?", antworten: [
+                    "3.25 €","3 €","2.75 €","2.65 €"
+                ], richtig: 1},
+                {text:"Das Ergebnis, wenn man alle Zahlen von 1 bis 100 addiert, lautet...", antworten: [
+                    "5050","6550","9950","10100"
+                ], richtig: 1},
+                {text:"767.376 ist das Quadrat einer natürlichen Zahl. Eine der folgenden drei Zahlen ist die richtige Lösung - welche?", antworten: [
+                    "862","868","871","876"
+                ], richtig: 4},
+                {text:"An einem Holzwürfel wird jede seiner acht Ecken abgesägt. Die Schnitte beginnen genau in der Mitte der Würfelkanten. Wie viele Ecken hat der so entstandene Körper?", antworten: [
+                    "8","12","16","24"
+                ], richtig: 2},
+                {text:"Wie lautet das Ergebnis einer beliebigen Zahl X nach folgenden Rechenschritten: ((X+11)*2-20)*5-10X", antworten: [
+                    "5","10","11","20"
+                ], richtig: 2},
+                {text:"Was ist die Summe der Quersummen der Zahlen von 1 bis 30?", antworten: [
+                    "149","155","168","210"
+                ], richtig: 3},
+                {text:"Punkt 15 Uhr stehen großer und kleiner Uhrzeiger genau im rechten Winkel zueinander. Zu welcher Zeit zwischen 15 und 16 Uhr ist das nochmals der Fall?", antworten: [
+                    "15:30 Uhr","15:33 Uhr","15:36 Uhr","15:40 Uhr"
+                ], richtig: 2}
+                ]}],
             Themen : ["Informatik", "Mathematik", "Chemie"],
             aktuelleFrage: 0,
             ausgewaehltesQuiz: 0,
