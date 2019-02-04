e54ae6f8db11272dd6c75813148914428bf1e4ca
Janning Vygen <vygen@kicktipp.de>
2019-02-04 11:52:22 +0100

    fix

--- quizapp-frontend/src/components/Antwort.vue
+++ quizapp-frontend/src/components/Antwort.vue
@@ -1,106 +1,111 @@
 <template>
-    <div class="antwortbox">
+  <div class="antwortbox">
         <span class="antwortUnterbox">
             <div class="antwortUL"
-                v-if="antwortKennung == antwort.id"
-                style="cursor: pointer"
-                @click="antwortGewaehlt(1)" 
-                :class="{gewaehltUL: this.gewaehlteAntwort==1, richtigUL: this.richtigeAntwort==1&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=1&&this.antwortUeberprueft}"
-                >{{ antwort.A1 }}
+                 v-if="antwortKennung == antwort.id"
+                 style="cursor: pointer"
+                 @click="antwortGewaehlt(1)"
+                 :class="{gewaehltUL: this.gewaehlteAntwort==1, richtigUL: this.richtigeAntwort==1&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=1&&this.antwortUeberprueft}"
+            >{{ antwort.A1 }}
                 </div>
-            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer" @click="antwortGewaehlt(2)" 
-                :class="{gewaehltUL: this.gewaehlteAntwort==2, richtigUL: this.richtigeAntwort==2&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=2&&this.antwortUeberprueft}">{{ antwort.A2 }}</div>
+            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer"
+                 @click="antwortGewaehlt(2)"
+                 :class="{gewaehltUL: this.gewaehlteAntwort==2, richtigUL: this.richtigeAntwort==2&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=2&&this.antwortUeberprueft}">{{ antwort.A2 }}</div>
+        </span> <span class="antwortUnterbox">
+            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer"
+                 @click="antwortGewaehlt(3)"
+                 :class="{gewaehltUL: this.gewaehlteAntwort==3, richtigUL: this.richtigeAntwort==3&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=3&&this.antwortUeberprueft}">{{ antwort.A3 }}</div>
+            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer"
+                 @click="antwortGewaehlt(4)"
+                 :class="{gewaehltUL: this.gewaehlteAntwort==4, richtigUL: this.richtigeAntwort==4&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=4&&this.antwortUeberprueft}">{{ antwort.A4 }}</div>
         </span>
-        <span class="antwortUnterbox">
-            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer" @click="antwortGewaehlt(3)" 
-                :class="{gewaehltUL: this.gewaehlteAntwort==3, richtigUL: this.richtigeAntwort==3&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=3&&this.antwortUeberprueft}">{{ antwort.A3 }}</div>
-            <div class="antwortUL" v-if="antwortKennung == antwort.id" style="cursor: pointer" @click="antwortGewaehlt(4)" 
-                :class="{gewaehltUL: this.gewaehlteAntwort==4, richtigUL: this.richtigeAntwort==4&&this.antwortUeberprueft, falschUL: this.richtigeAntwort!=4&&this.antwortUeberprueft}">{{ antwort.A4 }}</div>
-        </span>
-    </div>
+  </div>
 </template>
 
 <script>
-import { eventBus } from "../main.js";
-export default {
+  import {eventBus} from "../main.js";
+
+  export default {
     props: ['antwort', 'ausgewaehltesQuiz'],
-    data: function() {
-        return {
-            richtigeAntwort: 0,
-            aktuelleFrage: 0,
-            gewaehlteAntwort: 0,
-            antwortUeberprueft: false,
-            gameStarted: false
-        }
+    data: function () {
+      return {
+        richtigeAntwort: 0,
+        aktuelleFrage: 0,
+        gewaehlteAntwort: 0,
+        antwortUeberprueft: false,
+        gameStarted: false
+      }
     },
     computed: {
-        antwortKennung() {
-            return "" + this.ausgewaehltesQuiz + this.aktuelleFrage;
-        }
+      antwortKennung() {
+        return "" + this.ausgewaehltesQuiz + this.aktuelleFrage;
+      }
     },
     methods: {
-        antwortGewaehlt(ans) {
-            if (!this.antwortUeberprueft) {
-                this.gewaehlteAntwort = ans;
-                eventBus.$emit('antwortGewaehlt', this.gewaehlteAntwort)
-            }
+      antwortGewaehlt(ans) {
+        if (!this.antwortUeberprueft) {
+          this.gewaehlteAntwort = ans;
+          eventBus.$emit('antwortGewaehlt', this.gewaehlteAntwort)
         }
+      }
     },
     created() {
-        eventBus.$on('neueFrage', (aktuelleFrage) => {
-            this.aktuelleFrage = aktuelleFrage
-            this.gewaehlteAntwort = 0
-            this.richtigeAntwort = 0
-            this.antwortUeberprueft = false
-        }),
+      eventBus.$on('neueFrage', (aktuelleFrage) => {
+        this.aktuelleFrage = aktuelleFrage
+        this.gewaehlteAntwort = 0
+        this.richtigeAntwort = 0
+        this.antwortUeberprueft = false
+      }),
         eventBus.$on('antwortUeberprueft', (richtigeAntwort) => {
-            this.richtigeAntwort = richtigeAntwort
-            this.antwortUeberprueft = true
+          this.richtigeAntwort = richtigeAntwort
+          this.antwortUeberprueft = true
         }),
         eventBus.$on('gameStopped', () => {
-            this.gameStarted = false
-            this.antwortUeberprueft = false
-            this.aktuelleFrage = 0
-            this.gewaehlteAntwort = 0
+          this.gameStarted = false
+          this.antwortUeberprueft = false
+          this.aktuelleFrage = 0
+          this.gewaehlteAntwort = 0
         })
     },
     mounted() {
-        eventBus.$emit('ersteFrageAufrufen');
+      eventBus.$emit('ersteFrageAufrufen');
     }
-}
+  }
 </script>
 
 <style>
-.antwortbox {
+  .antwortbox {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
-    }
-.antwortUnterbox {
+  }
+
+  .antwortUnterbox {
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
-}
-.antwortUL {
+  }
+
+  .antwortUL {
     padding: 1em;
     margin: 1em;
     min-width: 200px;
     width: 300px;
     background-color: rgba(167, 167, 167, 0.5);
     border: 1px solid gray
-}
+  }
 
-.richtigUL {
+  .richtigUL {
     background-color: rgb(172, 255, 47) !important
-}
+  }
 
-.falschUL {
+  .falschUL {
     background-color: rgba(192, 28, 0, 0.9) !important
-}
+  }
 
-.gewaehltUL {
+  .gewaehltUL {
     background-color: rgba(57, 79, 178, 0.7);
     border: 3px double rgb(128, 128, 128)
-}
+  }
 </style>
--- quizapp-frontend/src/components/Fragen.vue
+++ quizapp-frontend/src/components/Fragen.vue
@@ -20,28 +20,16 @@ export default {
     data: function() {
         return {
             Fragen : [
-                {id: 11, Richtig: 2, Frage: "Was ist der Affengriff für Windows-Nutzer?"},
-                {id: 12, Richtig: 4, Frage: "Shareware ist ..."},
-                {id: 13, Richtig: 2, Frage: "Im Binärsystem gibt es ..."},
-                {id: 14, Richtig: 3, Frage: "Was ist ein Bit?"},
-                {id: 15, Richtig: 2, Frage: "Wie viele Zustände kann man mit zwei Bits herstellen?"},
-                {id: 16, Richtig: 1, Frage: "Weiter im Binär-/Dualsystem. Wie lautet die nächste Zahl in dieser Reihe? 0, 1, 10, 11, 100, 101, 110, 111, 1000, ..."},
-                {id: 17, Richtig: 4, Frage: "Dualsystem: 1111 -Dezimalsystem: ..."},
-                {id: 18, Richtig: 2, Frage: "Dualsystem: 10011 -Dezimalsystem: ..."},
-                {id: 19, Richtig: 4, Frage: "Das Hexadezimalsystem ist ein Stellenwertsystem zur Basis ..."},
-                {id: 110, Richtig: 3, Frage: "Rechne folgende Hexadezimalzahl in eine Dezimalzahl um: 1BF6ₕₑₓ"},
-                {id: 21, Richtig: 3, Frage: "Was ist die Summe der Zahlen von 1 bis 10?"},
-                {id: 22, Richtig: 4, Frage: "Sie stapeln fünf Würfel übereinander zu einem Turm. Die Augenzahl auf der Oberseite des obersten Würfels ist sechs. Wie lautet die Summe aller sichtbaren Augenzahlen?"},
-                {id: 23, Richtig: 3, Frage: "Es ist Punkt 14 Uhr. In welchem Winkel stehen großer und kleiner Zeiger in diesem Moment zueinander?"},
-                {id: 24, Richtig: 1, Frage: "300 g Erdbeeren kosten 1,95 €. Wie viel kosten 500 g?"},
-                {id: 25, Richtig: 1, Frage: "Das Ergebnis, wenn man alle Zahlen von 1 bis 100 addiert, lautet..."},
-                {id: 26, Richtig: 4, Frage: "767.376 ist das Quadrat einer natürlichen Zahl. Eine der folgenden drei Zahlen ist die richtige Lösung - welche?"},
-                {id: 27, Richtig: 2, Frage: "An einem Holzwürfel wird jede seiner acht Ecken abgesägt. Die Schnitte beginnen genau in der Mitte der Würfelkanten. Wie viele Ecken hat der so entstandene Körper?"},
-                {id: 28, Richtig: 2, Frage: "Wie lautet das Ergebnis einer beliebigen Zahl X nach folgenden Rechenschritten: ((X+11)*2-20)*5-10X"},
-                {id: 29, Richtig: 3, Frage: "Was ist die Summe der Quersummen der Zahlen von 1 bis 30?"},
-                {id: 210, Richtig: 2, Frage: "Punkt 15 Uhr stehen großer und kleiner Uhrzeiger genau im rechten Winkel zueinander. Zu welcher Zeit zwischen 15 und 16 Uhr ist das nochmals der Fall?"}
-                
-
+                {id: 1, Richtig: 2, Frage: "Was ist der Affengriff für Windows-Nutzer?"},
+                {id: 2, Richtig: 4, Frage: "Shareware ist ..."},
+                {id: 3, Richtig: 2, Frage: "Im Binärsystem gibt es ..."},
+                {id: 4, Richtig: 3, Frage: "Was ist ein Bit?"},
+                {id: 5, Richtig: 2, Frage: "Wie viele Zustände kann man mit zwei Bits herstellen?"},
+                {id: 6, Richtig: 1, Frage: "Weiter im Binär-/Dualsystem. Wie lautet die nächste Zahl in dieser Reihe? 0, 1, 10, 11, 100, 101, 110, 111, 1000, ..."},
+                {id: 7, Richtig: 4, Frage: "Dualsystem: 1111 -Dezimalsystem: ..."},
+                {id: 8, Richtig: 2, Frage: "Dualsystem: 10011 -Dezimalsystem: ..."},
+                {id: 9, Richtig: 4, Frage: "Das Hexadezimalsystem ist ein Stellenwertsystem zur Basis ..."},
+                {id: 10, Richtig: 3, Frage: "Rechne folgende Hexadezimalzahl in eine Dezimalzahl um: 1BF6ₕₑₓ"}
             ],
             Quiz : [{quizId:1, name: "Informatik", author: "Lennart G.", fragen: [
                 {text: "Was ist der Affengriff für Windows-Nutzer?", antworten: [
@@ -74,39 +62,7 @@ export default {
                 {text: "Rechne folgende Hexadezimalzahl in eine Dezimalzahl um: 1BF6ₕₑₓ", antworten: [
                     "32","1080","7158","23445"
                 ], richtig: 3}
-            ]},
-            {quizId:2, name: "Mathematik", author: "Lennart G.", fragen: [
-                {text:"Was ist die Summe der Zahlen von 1 bis 10?", antworten: [
-                    "48","50","55","58"
-                ], richtig: 3},
-                {text:"Sie stapeln fünf Würfel übereinander zu einem Turm. Die Augenzahl auf der Oberseite des obersten Würfels ist sechs. Wie lautet die Summe aller sichtbaren Augenzahlen?", antworten: [
-                    "61","66","71","76"
-                ], richtig: 4},
-                {text:"Es ist Punkt 14 Uhr. In welchem Winkel stehen großer und kleiner Zeiger in diesem Moment zueinander?", antworten: [
-                    "45 Grad","55 Grad","60 Grad","66.6 Grad"
-                ], richtig: 3},
-                {text:"300 g Erdbeeren kosten 1,95 €. Wie viel kosten 500 g?", antworten: [
-                    "3.25 €","3 €","2.75 €","2.65 €"
-                ], richtig: 1},
-                {text:"Das Ergebnis, wenn man alle Zahlen von 1 bis 100 addiert, lautet...", antworten: [
-                    "5050","6550","9950","10100"
-                ], richtig: 1},
-                {text:"767.376 ist das Quadrat einer natürlichen Zahl. Eine der folgenden drei Zahlen ist die richtige Lösung - welche?", antworten: [
-                    "862","868","871","876"
-                ], richtig: 4},
-                {text:"An einem Holzwürfel wird jede seiner acht Ecken abgesägt. Die Schnitte beginnen genau in der Mitte der Würfelkanten. Wie viele Ecken hat der so entstandene Körper?", antworten: [
-                    "8","12","16","24"
-                ], richtig: 2},
-                {text:"Wie lautet das Ergebnis einer beliebigen Zahl X nach folgenden Rechenschritten: ((X+11)*2-20)*5-10X", antworten: [
-                    "5","10","11","20"
-                ], richtig: 2},
-                {text:"Was ist die Summe der Quersummen der Zahlen von 1 bis 30?", antworten: [
-                    "149","155","168","210"
-                ], richtig: 3},
-                {text:"Punkt 15 Uhr stehen großer und kleiner Uhrzeiger genau im rechten Winkel zueinander. Zu welcher Zeit zwischen 15 und 16 Uhr ist das nochmals der Fall?", antworten: [
-                    "15:30 Uhr","15:33 Uhr","15:36 Uhr","15:40 Uhr"
-                ], richtig: 2}
-                ]}],
+            ]}],
             Themen : ["Informatik", "Mathematik", "Chemie"],
             aktuelleFrage: 0,
             ausgewaehltesQuiz: 0,
@@ -139,10 +95,10 @@ export default {
         antwortUeberpruefen() {
             this.antwortUeberprueft = true;
             if (this.aktuelleFrage!= 0) {
-            if (this.Fragen[(this.ausgewaehltesQuiz-1)*10+this.aktuelleFrage-1].Richtig == this.gewaehlteAntwort) {
+            if (this.Fragen[this.aktuelleFrage-1].Richtig == this.gewaehlteAntwort) {
                 this.richtigeAntworten++
             }  }
-            eventBus.$emit('antwortUeberprueft', this.Fragen[((this.ausgewaehltesQuiz-1)*10)+this.aktuelleFrage-1].Richtig)
+            eventBus.$emit('antwortUeberprueft', this.Fragen[this.aktuelleFrage-1].Richtig)
         },
     },
     created() {
